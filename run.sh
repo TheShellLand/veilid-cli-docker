6fcd7daf5bc27fb0d868fffc2b2eafc9e5d3be0b
naisanzaa <naisanza@gmail.com>
2023-11-24 21:15:07 -0800

    update veilid-cli

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 
 cd $(dirname $0); set -xe
 
-docker rm -f veilid-cli || true
+docker rm -f veilid-cli 2>/dev/null || true
 docker run -it --name veilid-cli ghcr.io/theshellland/veilid-cli:latest $@
