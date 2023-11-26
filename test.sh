f3c80e35c16df39a61b9982eb34c14d17e1b9b71
naisanzaa <naisanza@gmail.com>
2023-11-25 23:37:09 -0800

    add docker network and default connection to veilid-node:5959

--- test.sh
+++ test.sh
@@ -4,4 +4,6 @@
 
 cd $(dirname $0); set -xe
 
-docker run --rm -it --entrypoint bash ghcr.io/theshellland/veilid-cli:latest $@
+docker network create veilid || true
+docker rm -f veilid-cli 2>/dev/null || true
+docker run --rm -it --name veilid-cli --net veilid ghcr.io/theshellland/veilid-cli:latest $@
